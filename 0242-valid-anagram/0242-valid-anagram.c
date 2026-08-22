85dbef36774c57049d21fa754b8c56db43e0ddff
Akshath Chaudhary <akshathchaudhary10@gmail.com>
2026-08-23 03:47:32 +0530

    Time: 0 ms (100%), Space: 9 MB (34.76%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.c
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -1,11 +1,14 @@
 bool isAnagram(char* s, char* t) {
     int count[26]={0};
+    //counting number of elements in s
     for(int i=0;s[i]!='\0';i++){
         count[s[i]-'a']++;
     }
+    //deleting elements from count
     for(int i=0;t[i]!='\0';i++){
         count[t[i]-'a']--;
     }
+    //if count is 0 then both strings had same elements hence anagram..
     for(int i=0;i<26;i++){
         if(count[i]!=0){
             return false;
