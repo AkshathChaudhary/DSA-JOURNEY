fa5bdfbd76f080b42add01b37b9963765201d522
Akshath Chaudhary <akshathchaudhary10@gmail.com>
2026-08-23 22:44:37 +0530

    Time: 0 ms (100%), Space: 10.1 MB (8.96%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,19 @@
+int search(int* nums, int numsSize, int target) {
+    int left=0;
+    int right=numsSize-1;
+    
+    while(left<=right){
+        int mid=(left+right)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(target>nums[mid]){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+        
+    }
+    return -1;
+}
